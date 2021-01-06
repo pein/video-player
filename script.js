75c73547b069a868504f96c719a6cca0d92d93df
Ilyes Houha <mi.houha@gmail.com>
2021-01-06 17:23:22 +0100

    Play/Pause function

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-// Play & Pause ----------------------------------- //
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullScreenBn = document.querySelector(".fullscreen");
 
+// Play & Pause ----------------------------------- //
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
+function togglePlay() {
+  console.log("sss");
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+  }
+}
 // Progress Bar ---------------------------------- //
 
 // Volume Controls --------------------------- //
@@ -7,3 +33,9 @@
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
+
+// Event Listners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+// On video end
+video.addEventListener("ended", showPlayIcon);
